ffc45b74f0a0fabae9a7fa9acfaa0a37ecb875ca
KennethMontero256 <kenneth.m1394@gmail.com>
2016-09-09 14:26:03 -0600

    null

--- core/Data/Data.php
+++ core/Data/Data.php
@@ -5,7 +5,6 @@ class Data extends mysqli {
   public function __construct() {
     parent::__construct(DB_HOST,DB_USER,DB_PASS,DB_NAME);
     $this->connect_errno ? die('Error en la conexión a la base de datos') : null;
-    //$this->set_charset("utf8");
   }
 
   public function recorrer($query) {
