e93909ff6c3a43f400da9e83f06599e6b5604417
PaguaSS <pagua210@gmail.com>
2016-09-12 01:14:30 -0600

    archivos y fuentes, prototipo nav bar

---
+++ view/modules/barraNavegacion.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <link rel="stylesheet" type="text/css" href="../css/Roboto/WebFont/roboto_thin_macroman/stylesheet.css">
+    <link rel="stylesheet" type="text/css" href="../css/estiloBarraNavegacion.css">
+  </head>
+  <body>
+	  <nav>
+	    <div class="contenedor_nav">
+		  <div class="logo-empresa">
+			 <a href="#">El Tostador</a>
+		  </div>
+		  <div class="opciones">
+			<ul>
+				<li><a href="#">Caja</a></li>
+				<li><a href="#">Inventario</a></li>
+				<li><a href="#">Pedidos</a></li>
+				<li><a href="#">Nombre usuario</a>
+					<ul>
+						<li><a href="#">Cambiar contraseña</a></li>
+						<li><a href="#">Cerrar sesión</a></li>
+					</ul>
+				</li>
+			</ul>
+		  </div>
+		  </div>
+	  </nav>
+  </body>
+</html>
