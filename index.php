56dd047affef303aaf4e6c659f989266245871da
KennethMontero256 <kenneth.m1394@gmail.com>
2016-09-12 16:44:09 -0600

    correcciones

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
     require("core/core.php");
     if (isset($_GET['clase'])){
-      if (file_exists("core/controller/".strtolower($_GET['clase']).".php")) {
-        include("core/controller/".strtolower($_GET['clase']).".php");
+      if (file_exists("core/Business/".strtolower($_GET['clase']).".php")) {
+        include("core/Business/".strtolower($_GET['clase']).".php");
       }
       else {
         include("view/modules/error.php");
