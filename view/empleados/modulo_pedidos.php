7fb93423f85ab6b81e8241231eb29388f9e257bb
PaguaSS <pagua210@gmail.com>
2016-09-12 23:49:16 -0600

    actualizacion

--- view/empleados/modulo_pedidos.php
+++ view/empleados/modulo_pedidos.php
@@ -8,16 +8,50 @@
 </head>
 <body>
 	<?php
-	include("barraNavegacionPrincipal.php");
+		include("barraNavegacionPrincipal.php");
 	?>
 	<div class="menuLateral">
+		<p>Sección de:</p>
 		<ul>
-			<li><a href="#" id="opPedidoEspera"></a>Pedidos en espera</li>
-			<li><a href="#" id="opPedidoReal"></a>Pedidos realizados</li>
+			<li><a href="#" id="opPedidoEspera">Pedidos en espera</a></li>
+			<li><a href="#" id="opPedidoReal">Pedidos realizados</a></li>
 		</ul>
 	</div>
-	<div class="contenido">
-			
+	<div class="contenedorPedidos">
+		<div class="contenidoPedidos">
+				
+		</div>
+	</div>
+	<div class="contenedorModal">
+		<form class="frmPedido" action="" method="get" accept-charset="utf-8">
+			<table>
+				<caption>Pedido #:000987</caption>
+				<thead>
+					<tr>
+						<th>Sucursal: Cariari</th>
+						<th>Fecha-hora: 12/09/2016 23:40 p.m.</th>
+						<th></th>
+					</tr>
+					<tr>
+						<th>Empleado: Steven Mendez</th> 
+						<th></th>
+						<th></th>
+					</tr>
+					<tr>
+						<th colspan="" rowspan="" headers="" scope="">Código</th>
+						<th colspan="" rowspan="" headers="" scope="">Nombre</th>
+						<th colspan="" rowspan="" headers="" scope="">Cantidad</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<td>00099</td>
+						<td>Mani salado</td>
+						<td>5300k</td>
+					</tr>
+				</tbody>
+			</table>
+		</form>
 	</div>
 	<a class='flotante' href='#' >Add</a>
 </body>
