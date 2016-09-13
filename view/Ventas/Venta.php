1b61d3aa5e78f1dc1b7ff46c963aebd1b27634f7
Cafej <bernardofdez2011@gmail.com>
2016-09-13 01:16:29 -0600

    Mucho Javascript

--- view/Ventas/Venta.php
+++ view/Ventas/Venta.php
@@ -28,6 +28,52 @@ and open the template in the editor.
         
         ?>
         
+          <script>   
+                        var i = 0;
+
+function duplicate2(){
+    var myDiv = document.getElementById("producto0"); //Obtiene el objeto por el id
+    var divPops = document.getElementById("detalle"); //Obtiene le padre del div
+    var divClone = myDiv.cloneNode(true); // crea un clon profundo
+    divClone.id = "producto" + ++i; // mantiene un contador para crear divs con id diferente
+    divPops.appendChild(divClone); //agrega el clone al padre
+    
+    hijos = divClone.childNodes;
+    for (j = 0; j < hijos.length; j++) { 
+        hijos[j].id = hijos[j].id + i; 
+    }
+    
+}
+
+function getValueComboToInput(){
+    var combo = document.getElementById("comboProductos");
+    var value = combo.options[combo.selectedIndex].value;
+    var input = document.getElementById("codigoProducto");
+    input.value = value;
+    
+}
+
+function deleteDiv(e){
+    
+    
+    var eId = e.id;
+    var vector = eId.split("");
+    var numero = vector[vector.length-1];
+    //var divpops = document.getElementById("producto"+numero);
+    //window.alert(divpops.id);
+    document.getElementById("producto"+numero).remove();
+    
+}
+
+function delete_row(e)
+{
+    e.parentNode.removeChild(e.parentNode);
+}
+
+
+            </script> 
+        
+        
         <div id="contenedorVenta">
 
             <div id="informacionVenta">
@@ -46,28 +92,18 @@ and open the template in the editor.
 
             </div>
 
-            <script>   
-                        var i = 0;
-
-function duplicate2(){
-    var myDiv = document.getElementById("producto0"); //Obtiene el objeto por el id
-    var divPops = document.getElementById("detalle"); //Obtiene le padre del div
-    var divClone = myDiv.cloneNode(true); // crea un clon profundo
-    divClone.id = "producto" + ++i; // mantiene un contador para crear divs con id diferente
-    divPops.appendChild(divClone); //agrega el clone al padre
-}
-            </script> 
+          
             
             
             <div id="detalle">
                 <div id="producto0">
 
-                    <label>Codigo producto:</label>
-                    <input type="text" value="">
+                    <label id="lbCod">Codigo producto:</label>
+                    <input id="cod" type="text" value="" id="codigoProducto">
                     
-                    <label>Nombre prodcuto:</label>
+                    <label id="lbNombre">Nombre producto:</label>
                     
-                    <select>
+                    <select id="comboProductos" onclick="getValueComboToInput()">
                         <ul>
                             <?php
                             for ($i = 0; $i < count($productos); $i++) {
@@ -77,13 +113,13 @@ function duplicate2(){
                         </ul>
                     </select>
                     
-                    <label>Cantidad</label>
-                    <input type="number">
+                    <label id="lbCantidad">Cantidad</label>
+                    <input id="cantdad" type="number">
                     
-                    <label>Total Linea</label>
-                    <input type="number" value="" readonly>
+                    <label id="lbTotal">Total Linea</label>
+                    <input id="total" type="number" value="" readonly>
                     
-                    
+                    <input id="delete" type="button" value="X" onclick="deleteDiv(this)">
                 </div>
                 
                 
