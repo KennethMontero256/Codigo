16db591c38bae911f76b6471dc2408543abf4c59
PaguaSS <pagua210@gmail.com>
2016-09-21 13:44:50 -0600

    update code

--- Data/Data.php
+++ Data/Data.php
@@ -31,32 +31,3 @@ function getConnection(){
 
 
 ?>
-=======
-<?php
-
-class Data extends mysqli {
-
-  public function __construct() {
-    parent::__construct(DB_HOST,DB_USER,DB_PASS,DB_NAME);
-    $this->connect_errno ? die('Error en la conexión a la base de datos') : null;
-  }
-
-  public function recorrer($query) {
-    return mysqli_fetch_array($query);
-  }
-
-}
-
-function getConnection(){
-
-    $mysqli = new mysqli('68.178.217.43', 'ucrgrupo4', 'Grupo#4LkF!', 'ucrgrupo4');
-
-    if ($mysqli->connect_error) {
-        die("Connection failed: " . $mysqli->connect_error);
-    }
-    return $mysqli;
-}
-
-
-?>
->>>>>>> 1dc226a5b89c9b22e6ad3407a73fdc116463f5c7
