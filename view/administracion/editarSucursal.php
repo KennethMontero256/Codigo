b671a6cdc3f93da61593acf83b59d68ac71817dc
KennethMontero256 <kenneth.m1394@gmail.com>
2016-09-21 00:57:04 -0600

    Editar sucursal...

--- view/administracion/editarSucursal.php
+++ view/administracion/editarSucursal.php
@@ -6,12 +6,16 @@
   </head>
   <body>
     <form class="" action="index.html" method="post">
-      <?php
-        if ($reg=mysqli_fetch_array($sucursal)) {
-          echo $reg['id'];
-        }
-       ?>
-      <input type="text" name="name" value="$sucursal[]">
+      <?php if ($reg=mysqli_fetch_array($sucursal)){ ?>
+
+          <label>Nombre:</label>
+          <input type="text" name="name" value="<?php  echo $reg['nombre']; ?>">
+          <label>Dirección:</label>
+          <input type="text" name="name" value="<?php  echo $reg['direccion']; ?>">
+          <label>Teléfono:</label>
+          <input type="text" name="name" value="<?php  echo $reg['telefono']; ?>">
+          <a href="#" class="editar"></a>
+      <?php } ?>
     </form>
   </body>
 </html>
