02b657bc6bc082e1153472819e99e1e8af7d11a6
KennethMontero256 <kenneth.m1394@gmail.com>
2016-09-20 23:01:31 -0600

    Administrar sucursal

--- js/funcionesAdministrador.js
+++ js/funcionesAdministrador.js
@@ -1,14 +1,24 @@
 
 $(document).ready(function() {
     $(".012").click(function (e) {
-      alert("K");
-        e.preventDefault();
-        listaSucursales(this.getAttribute("href"));
+      alert("");
+      e.preventDefault();
+      listaSucursales(this.getAttribute("href"));
+    });
+    $(".btnEditSucr").click(function (e) {
+      e.preventDefault();
+      editarSucursal(this.getAttribute("href"));
     });
 });
 
 function listaSucursales(accion){
   var capa = document.getElementById("contenedor");
   document.getElementById("contenedor").innerHTML = "Cargando...";
+  $(capa).load("?clase=sucursalController&&accion="+accion);
+}
+
+function editarSucursal(accion){
+  var capa = document.getElementById("contenedorLista");
+  document.getElementById("contenedorLista").innerHTML = "Cargando...";
   $(capa).load("?clase=sucursalController&&accion="+accion);
 }
