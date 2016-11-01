7aa4a740ca3dfdcee6b513676ea18ee6d6bb31ef
PaguaSS <pagua210@gmail.com>
2016-11-01 16:26:10 -0600

    Configuracion test

--- tests/CategoriaUnitTest.php
+++ tests/CategoriaUnitTest.php
@@ -1,17 +1,13 @@
 <?php
-	require "../Data/DataCategoria.php";
-	require "../Domain/Categoria.php";
+	require "Data/DataCategoria.php";
+	require "Domain/Categoria.php";
 
-	class CategoriaUnitTest extends PHPUnit_FrameWokr_UnitTestCase{
-		private $dataCategoria;
-
-		public function setUp(){
-			$this->dataCategoria = new DataCategoria();
-		}
-
-		public function insertarTest(){
-			$respuesta = $this->dataCategoria->agregarActualizarCategoria(new Categoria(0,"pruebaTest"));
-			$this->assertEqual(true, $respuesta);
+	class CategoriaUnitTest extends PHPUnit_Framework_TestCase{
+	
+		public function testInsertar(){
+			$dataCategoria = new DataCategoria();
+			$respuesta = $dataCategoria->agregarActualizarCategoria(new Categoria(0,"NuevaCategoria"));
+			$this->assertEquals(true, $respuesta);
 		}
 
 	}
